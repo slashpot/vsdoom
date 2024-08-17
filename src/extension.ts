509281c7e70a97e68b9ee7aeb3ce6f35005be970
Eric Lin <tttttt123465@gmail.com>
2024-08-17 21:43:53 +0800

    save game WIP

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,14 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
+import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
+  let panel: vscode.WebviewPanel | undefined = undefined;
+
   context.subscriptions.push(
     vscode.commands.registerCommand("vsdoom.start", () => {
       // Create and show a new webview
-      const panel = vscode.window.createWebviewPanel(
+      panel = vscode.window.createWebviewPanel(
         "vsdoom", // Identifies the type of the webview. Used internally
         "VsDoom", // Title of the panel displayed to the user
         vscode.ViewColumn.One, // Editor column to show the new webview panel in.
@@ -43,7 +46,7 @@ export function activate(context: vscode.ExtensionContext) {
       );
       const wadSrc = panel.webview.asWebviewUri(wadPath);
 
-	  const cfgPath = vscode.Uri.joinPath(
+      const cfgPath = vscode.Uri.joinPath(
         context.extensionUri,
         "src",
         "doom",
@@ -58,11 +61,52 @@ export function activate(context: vscode.ExtensionContext) {
       );
 
       htmlContent = htmlContent
-	  .replace("{{jsSrc}}", jsSrc.toString())
-	  .replace("{{wad}}", wadSrc.toString())
-	  .replace("{{cfg}}", cfgSrc.toString());
+        .replace("{{jsSrc}}", jsSrc.toString())
+        .replace("{{wad}}", wadSrc.toString())
+        .replace("{{cfg}}", cfgSrc.toString());
 
       panel.webview.html = htmlContent;
+
+	  panel.webview.onDidReceiveMessage(
+        message => {
+            switch (message.command) {
+                case 'saveGame':
+					console.log('message', message);
+                    const workspaceFolders = vscode.workspace.workspaceFolders;
+                    if (!workspaceFolders) {
+                        vscode.window.showErrorMessage("Working folder not found, can't save game.");
+                        return;
+                    }
+                    const filePath = path.join(workspaceFolders[0].uri.fsPath, 'doomSave.bin');
+                    
+                    // Convert Base64 back to binary
+                    const binaryData = Buffer.from(message.data, 'base64');
+					console.log('binaryData: ', binaryData);
+                    
+                    fs.writeFile(filePath, binaryData, err => {
+                        if (err) {
+                            vscode.window.showErrorMessage("Failed to save the game.");
+                        } else {
+                            vscode.window.showInformationMessage("Game saved successfully.");
+                        }
+                    });
+                    return;
+            }
+        },
+        undefined,
+        context.subscriptions
+    );
+    })
+  );
+
+  // Our new command
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vsdoom.savegame", () => {
+      if (!panel) {
+        return;
+      }
+
+      panel.webview.postMessage({ command: "save game" });
     })
   );
 }
